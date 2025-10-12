f1c7a68dd9297953d902ec858ba9c7536a3ef9a4
aarsenovic <acawarrior@gmail.com>
2025-10-12 23:00:53 +0200

    feat: start building UI

---
+++ src/ui.js
@@ -0,0 +1,30 @@
+function createElement(tag, { text, classes = [], children = [],  attrs = {}} = {}) {
+  const el = document.createElement(tag);
+  if (text) el.textContent = text;
+  if (classes.length) el.classList.add(...classes);
+  children.forEach(child => el.appendChild(child));
+
+    if (attrs && typeof attrs === "object") {
+    for (const [key, value] of Object.entries(attrs)) {
+      el.setAttribute(key, value);
+    }
+  }
+
+  return el;
+}
+
+
+
+
+function createHeader() {
+    return createElement("header", {classes:["header"]})
+}
+
+
+
+function createMainContent () {
+    const sidebar = createElement("aside", {classes:["sidebar"]})
+    const main = createElement("main", {classes:["main-content"]})
+
+    return createElement("div", {classes:["container"], children:{sidebar,main}})
+}
