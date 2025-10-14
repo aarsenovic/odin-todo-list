f1a9f2d6889bf7c9b6fccb8945eab5cab1ad5425
aarsenovic <acawarrior@gmail.com>
2025-10-14 22:37:06 +0200

    feat: add sketch of modal for adding new project

--- src/ui.js
+++ src/ui.js
@@ -16,10 +16,21 @@ function createMainContent () {
 }
 
 function createAddProjectButton() {
-   
    const buttonText = "+";
    return createElement("button", {text:buttonText, classes:["add-new-project-button"]});
 
+}
+
+function createModalForAddNewProject() {
+  return createElement("dialog",{text: "radi", classes:["add-new-project-modal"]});
+}
+
+
+function connectModalToAddNewProjectButton() {
+  const addNewProjectButton = document.querySelector(".add-new-project-button");
+  const dialog = document.querySelector("dialog")
+  
+  addNewProjectButton.addEventListener("click", ()=>{dialog.showModal()})
 }
 
 
@@ -29,6 +40,8 @@ function createMainPage() {
   root.appendChild(createMainContent());
   const sidebar = document.querySelector(".sidebar");
   sidebar.appendChild(createAddProjectButton());
+  sidebar.appendChild(createModalForAddNewProject());
+  connectModalToAddNewProjectButton()
 }
 
 
